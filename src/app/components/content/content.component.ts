abea773dc5c2eed36801cdbfff7992f62f24da07
Miquéias Alves <miqueiasalves.developer@gmail.com>
2024-10-20 17:18:48 -0300

    feat: add about page html structure

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-content',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './content.component.html',
   styleUrl: './content.component.css'
 })
