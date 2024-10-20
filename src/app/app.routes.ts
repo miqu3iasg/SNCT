474df112cfef123b7a7f895d92a0a5e911439f21
Miquéias Alves <miqueiasalves.developer@gmail.com>
2024-10-20 17:16:53 -0300

    chore: add routes application

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { CompleteMaterialsComponent } from './components/complete-materials/complete-materials.component';
+import { ContentComponent } from './components/content/content.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: ContentComponent},
+  { path: 'sobre', component: AboutComponent},
+  { path: 'materiais', component: CompleteMaterialsComponent}
+];
